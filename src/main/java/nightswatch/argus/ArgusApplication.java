9042b6eef64247b9750991626dcb8b08964fce74
MD ZIYAD HUSSAIN <ziyadhussain23@gmail.com>
2026-01-12 11:42:23 +0530

    initialize Argus application with Maven setup and basic configuration

---
+++ src/main/java/nightswatch/argus/ArgusApplication.java
@@ -0,0 +1,13 @@
+package nightswatch.argus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nightswatch/argus/ArgusApplicationTests.java
@@ -0,0 +1,13 @@
+package nightswatch.argus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
