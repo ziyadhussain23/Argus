a2deeec1571037719d5d6563b5e5c7a6047746a4
MD ZIYAD HUSSAIN <ziyadhussain23@gmail.com>
2026-01-17 02:24:12 +0530

    Tighten WebSocket allowed origins to known frontend hosts

--- src/main/java/nightswatch/argus/config/WebSocketConfig.java
+++ src/main/java/nightswatch/argus/config/WebSocketConfig.java
@@ -22,7 +22,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // WebSocket endpoint for clients to connect
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+            .setAllowedOrigins(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "http://localhost:3000",
+                "http://127.0.0.1:3000"
+            )
                 .withSockJS();
     }
 }
