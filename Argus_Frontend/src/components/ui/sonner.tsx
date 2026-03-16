c475fef03084e34dae9acb623fe17a5796daf6af
mahipal1008 <mahipalparegi@gmail.com>
2026-03-16 09:17:29 +0530

    feat: add sonner toast UI component

--- Argus_Frontend/src/components/ui/sonner.tsx
+++ Argus_Frontend/src/components/ui/sonner.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "next-themes";
+import { useTheme } from "@/contexts/ThemeContext";
 import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
