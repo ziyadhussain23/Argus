cd3eeb81e635c5a72f482d0616e2adbbdb80c219
MD ZIYAD HUSSAIN <ziyadhussain23@gmail.com>
2026-03-23 10:17:28 +0530

    dto: Create ChangePasswordRequest

---
+++ src/main/java/nightswatch/argus/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,22 @@
+package nightswatch.argus.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    private String newPassword;
+}
