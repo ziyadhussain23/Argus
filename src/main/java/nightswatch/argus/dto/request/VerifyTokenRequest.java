3cb5e96004c3eefa6c7dffa52f51e99e7b26b583
MD ZIYAD HUSSAIN <ziyadhussain23@gmail.com>
2026-03-23 10:17:28 +0530

    dto: Create VerifyTokenRequest

---
+++ src/main/java/nightswatch/argus/dto/request/VerifyTokenRequest.java
@@ -0,0 +1,19 @@
+package nightswatch.argus.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VerifyTokenRequest {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+}
